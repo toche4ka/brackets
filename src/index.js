dc579d178aaf09fd60161b1bc8b96b64a023ec7e
Anna Tochk <anitochka1105@gmail.com>
2024-09-19 23:47:04 +0500

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for(let i=0; i<str.length; i++) {
+	let currentSimbol=str[i];
+
+	if(bracketsConfig.includes(currentSimbol)) {
+		stack.push(currentSimbol);
+	} else {
+		if (str.length===0) {
+			return false;
+		}
+		let topElem = stack[stack.length -1];
+
+		if (bracketsConfig[currentSimbol]===topElem) {
+			stack.pop();
+		} else {
+			return true;
+		}
+	}
+  }
+  return stack.length===0;
 }
